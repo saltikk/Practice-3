3926f23367e0c24ec8018e803bf900e0b05e0e5a
Saltanat <saltanatadilbekova387@gmail.com>
2026-02-21 14:40:22 +0500

    practice

---
+++ classes-objects.py
@@ -0,0 +1,31 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Emil", 36)
+
+print(p1.name)
+print(p1.age)
+
+
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
---
+++ functions.py
@@ -0,0 +1,30 @@
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
